89f3d1d650275d3c191655a8b73e45ed5be0dca2
David Raznick <kindly@gmail.com>
2021-09-07 12:56:49 +0100

    fix planit load script

--- scrapers/planit_load.sql
+++ scrapers/planit_load.sql
@@ -1,3 +1,4 @@
 create table planit_load(key int, file text, data jsonb);
 \copy planit_load from output/all.csv with CSV
+SELECT AddGeometryColumn ('public','planit_load','geom',4326,'POINT',2);
 update planit_load set geom = ST_MakePoint((data ->> 'location_x')::float, (data ->> 'location_y')::float);
